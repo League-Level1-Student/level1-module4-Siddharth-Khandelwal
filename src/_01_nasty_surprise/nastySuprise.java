cfe20684c9770161ef10e44e1d53eb5d96e26d1e
league <league@imac-14.attlocal.net>
2023-08-12 16:31:21 -0700

    nasty suprise

--- src/_01_nasty_surprise/nastySuprise.java
+++ src/_01_nasty_surprise/nastySuprise.java
@@ -1,5 +1,41 @@
 package _01_nasty_surprise;
 
-public class nastySuprise {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class nastySuprise implements ActionListener {
+	JFrame frame = new JFrame("Trick or Treat");
+	JPanel panel = new JPanel();
+	JButton trick = new JButton("Trick");
+	JButton treat = new JButton("Treat");
+	
+public void run() {
+	frame.add(panel);
+	panel.add(trick);
+	panel.add(treat);
+	frame.setVisible(true);
+	frame.pack();
+	trick.addActionListener(this);
+	treat.addActionListener(this);
+}
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	JButton pressed = (JButton) e.getSource();
+if(pressed == trick) {
+	System.out.println("hi");
+}
+if(pressed == treat) {
+	System.out.println("hi");
+}
+
+}
+	
 
 }
--- src/_01_nasty_surprise/nastySupriseRunner.java
+++ src/_01_nasty_surprise/nastySupriseRunner.java
@@ -2,6 +2,7 @@ package _01_nasty_surprise;
 
 public class nastySupriseRunner {
 public static void main(String[] args) {
-	
+	nastySuprise ns = new nastySuprise();
+	ns.run();
 }
 }
