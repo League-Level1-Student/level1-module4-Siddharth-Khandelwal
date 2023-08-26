a3cffd8320178c88b48a26befe2e697cae401672
league <league@imac-14.attlocal.net>
2023-08-26 16:26:06 -0700

    finish nasty suprise

--- src/_01_nasty_surprise/nastySuprise.java
+++ src/_01_nasty_surprise/nastySuprise.java
@@ -2,9 +2,14 @@ package _01_nasty_surprise;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
@@ -13,7 +18,19 @@ public class nastySuprise implements ActionListener {
 	JPanel panel = new JPanel();
 	JButton trick = new JButton("Trick");
 	JButton treat = new JButton("Treat");
-	
+	private void showPictureFromTheInternet(String imageUrl) {
+	    try {
+	        URL url = new URL(imageUrl);
+	        Icon icon = new ImageIcon(url);
+	        JLabel imageLabel = new JLabel(icon);
+	        JFrame frame = new JFrame();
+	        frame.add(imageLabel);
+	        frame.setVisible(true);
+	        frame.pack();
+	    } catch (MalformedURLException e) {
+	        e.printStackTrace();
+	    }
+	}
 public void run() {
 	frame.add(panel);
 	panel.add(trick);
@@ -22,6 +39,7 @@ public void run() {
 	frame.pack();
 	trick.addActionListener(this);
 	treat.addActionListener(this);
+
 }
 
 @Override
@@ -29,10 +47,10 @@ public void actionPerformed(ActionEvent e) {
 	// TODO Auto-generated method stub
 	JButton pressed = (JButton) e.getSource();
 if(pressed == trick) {
-	System.out.println("hi");
+showPictureFromTheInternet("https://di7dud5r8j0ls.cloudfront.net/fit-in/600x480/wordpress/wp-content/uploads/2016/07/Cover-Image1.jpg");	
 }
 if(pressed == treat) {
-	System.out.println("hi");
+showPictureFromTheInternet("https://wallpapers.com/images/hd/scary-face-pictures-i7u89wtc6niurxru.jpg");
 }
 
 }
