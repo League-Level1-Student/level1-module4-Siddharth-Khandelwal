00bcc17e957b2a75a53b3e6c38720955c53dfa46
league <karinabriellen@gmail.com>
2023-12-23 16:22:35 -0800

    Practice Quiz done, Slot machine started

---
+++ src/_12_slot_machine/SlotMachine.java
@@ -0,0 +1,20 @@
+package _12_slot_machine;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class SlotMachine {
+JFrame frame = new JFrame();
+JPanel panel = new JPanel();
+JLabel uno = new JLabel();
+JLabel dos = new JLabel();
+JLabel tres = new JLabel();
+JButton spin = new JButton("SPIN");
+	public void run() {
+		
+		
+	}
+
+}
---
+++ src/_12_slot_machine/SlotMachineRunner.java
@@ -0,0 +1,9 @@
+package _12_slot_machine;
+
+public class SlotMachineRunner {
+public static void main(String[] args) {
+SlotMachine r  = new SlotMachine();
+r.run();
+}
+}
+
